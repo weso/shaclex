934df9170f2c9d3b55afa549914ee52eb5d9f473
labra <jelabra@gmail.com>
2016-05-11 07:59:35 +0200

    Updated to version 0.0.1

--- update.bat
+++ update.bat
@@ -1,17 +1,17 @@
-set oldversion=0.2.2
-set version=0.7.6
+set oldversion=0.0.0
+set version=0.0.1
 
-set source=\src\shapes\ShExcala\target\universal
-set dest=\gh-pages\ShExcala\binaries
+set source=\src\shapes\shaclex\target\universal
+set dest=\gh-pages\shaclex\binaries
 
-rm %dest%\shexcala-%oldversion%.zip
-rm %dest%\shexcala-%oldversion%.tgz
-cp %source%\shexcala-%version%.zip %dest%\shexcala-%version%.zip
-cp %source%\shexcala-%version%.tgz %dest%\shexcala-%version%.tgz
+rm %dest%\shaclex-%oldversion%.zip
+rm %dest%\shaclex-%oldversion%.tgz
+cp %source%\shaclex-%version%.zip %dest%\shaclex-%version%.zip
+cp %source%\shaclex-%version%.tgz %dest%\shaclex-%version%.tgz
 sed -i "s|%oldversion%|%version%|g" index.html
 chmod +w index.html
 rm -rf latest/api/*
-cp -R /src/shapes/shexcala/target/scala-2.11/api/* latest/api
+cp -R /src/shapes/shaclex/target/scala-2.11/api/* latest/api
 git add -A
 git commit -m "Updated to version %version%"
 git push
