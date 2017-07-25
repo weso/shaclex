dd23115e08b05ad070bce2943c7cf8465fd5deb6
Jose Emilio Labra Gayo <labra@uniovi.es>
2017-07-25 16:08:27 +0200

    Added test of dependency graphs

--- modules/shex/src/test/scala/es/weso/shex/dependenciesTest.scala
+++ modules/shex/src/test/scala/es/weso/shex/dependenciesTest.scala
@@ -16,54 +16,86 @@ import scala.util.{Failure, Success}
 
 class dependenciesTest extends FunSpec with Matchers with EitherValues {
 
-  def negCyclesTest(schema: String, negCyclesLabels: Set[Set[String]]): Unit = {
-    val negCyclesExpected: Set[Set[IRILabel]] = negCyclesLabels.map((vs: Set[String]) => vs.map((s: String) => IRILabel(IRI(s))))
-    it(s"should check that negCycles of $schema are $negCyclesLabels") {
-      Schema.fromString(schema, "SHEXC")
-      match {
-        case Failure(e) => fail(s"Error $e parsing $schema")
-        case Success(schema) => {
-          schema.negCycles match {
-            case Left(msg) => fail(s"Error $msg calculating negative cycles of $schema")
-            case Right(cycles) => (negCyclesLabels.isEmpty, cycles.isEmpty) match {
-              case (true, true) => info("No neg cycles as expected")
-              case (true, false) => {
-//                val showCycles = cycles.map(_.map(_.id.map(_.toString).getOrElse("?")))
-                val graphStr = schema.depGraph.map(_.showEdges()).getOrElse("<empty>")
-                info(s"Dependency graph = ${graphStr}")
-                fail(s"Expected no negCycles but found neg cycles: $cycles")
-              }
-              case (false, true) => {
-                val graphStr = schema.depGraph.map(_.showEdges()).getOrElse("<empty>")
-                info(s"Dependency graph = ${graphStr}")
-                fail(s"Expected negCycles to be $negCyclesLabels but found no neg cycles")
-              }
-              case (false, false) => {
-                cycles should contain theSameElementsAs (negCyclesExpected)
+  describe("Dependency graph") {
+
+    def depGraphTest(schema: String, expectedGraph: Set[(String, Set[String])]): Unit = {
+      it(s"should check that dependency graph of $schema matches $expectedGraph") {
+        Schema.fromString(schema, "SHEXC") match {
+          case Failure(e) => fail(s"Error $e parsing $schema")
+          case Success(schema) => {
+            schema.depGraph match {
+              case Left(msg) => fail(s"Error $msg calculating dependency graph")
+              case Right(depGraph) => {
+                info(s"Dependency graph: ${depGraph.showEdges()}")
               }
             }
           }
         }
       }
     }
+    val e = "http://example.org/"
+    val s = e + "S"
+    val t = e + "T"
+   depGraphTest(
+     s"""
+       |prefix : <$e>
+       |
+       |:S { :p @:T }
+     """.stripMargin, Set((s, Set(t))))
   }
 
-/*   negCyclesTest(
-     """
-         |prefix : <http://example.org/>
-         |
-         |:S NOT @:T
-         |:T NOT @:S
-       """.stripMargin,
-     Set(Set("http://example.org/S","http://example.org/T"))
-   ) */
+  describe("Negative cycles test") {
 
-  negCyclesTest(
-    """
+    def negCyclesTest(schemaStr: String, negCyclesLabels: Set[Set[String]]): Unit = {
+      val negCyclesExpected: Set[Set[IRILabel]] = negCyclesLabels.map((vs: Set[String]) => vs.map((s: String) => IRILabel(IRI(s))))
+      it(s"should check that negCycles of $schemaStr are $negCyclesLabels") {
+        Schema.fromString(schemaStr, "SHEXC")
+        match {
+          case Failure(e) => fail(s"Error $e parsing $schemaStr")
+          case Success(schema) => {
+            schema.negCycles match {
+              case Left(msg) => fail(s"Error $msg calculating negative cycles of $schema")
+              case Right(cycles) => (negCyclesLabels.isEmpty, cycles.isEmpty) match {
+                case (true, true) => info("No neg cycles as expected")
+                case (true, false) => {
+                  //                val showCycles = cycles.map(_.map(_.id.map(_.toString).getOrElse("?")))
+                  val graphStr = schema.depGraph.map(_.showEdges()).getOrElse("<empty>")
+                  info(s"Dependency graph = ${graphStr}")
+                  fail(s"Expected no negCycles but found neg cycles: $cycles")
+                }
+                case (false, true) => {
+                  val graphStr = schema.depGraph.map(_.showEdges()).getOrElse("<empty>")
+                  info(s"Dependency graph = ${graphStr}")
+                  fail(s"Expected negCycles to be $negCyclesLabels but found no neg cycles")
+                }
+                case (false, false) => {
+                  cycles should contain theSameElementsAs (negCyclesExpected)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    negCyclesTest(
+      """
       |prefix : <http://example.org/>
       |
       |:S { :p @:T }
       |:T { :p @:S }
-    """.stripMargin,Set())
+    """.
+        stripMargin, Set())
+
+    negCyclesTest(
+      """
+      |prefix : <http://example.org/>
+      |
+      |:S { :p @:T } AND NOT @:R
+      |:T { :p @:S }
+    """.
+        stripMargin, Set())
+
+}
 
 }
