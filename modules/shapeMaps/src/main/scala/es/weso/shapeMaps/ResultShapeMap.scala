7327776ee9bdd06d2ab432bc8ea60a32db7f8a03
Labra <jelabra@gmail.com>
2017-09-21 00:31:39 +0200

    Repaired result shape map

--- modules/shapeMaps/src/main/scala/es/weso/shapeMaps/ResultShapeMap.scala
+++ modules/shapeMaps/src/main/scala/es/weso/shapeMaps/ResultShapeMap.scala
@@ -2,10 +2,40 @@ package es.weso.shapeMaps
 
 import es.weso.rdf.nodes.RDFNode
 
-case class ResultShapeMap(override val map: Map[RDFNode, Map[ShapeMapLabel, Info]]) extends FixedShapeMap(map) {
+case class ResultShapeMap(map: Map[RDFNode, Map[ShapeMapLabel, Info]]) extends ShapeMap {
   def addNodeAssociations(node: RDFNode, mapLabels: Map[ShapeMapLabel, Info]): ResultShapeMap = {
     ResultShapeMap(map.updated(node, map(node) ++ mapLabels))
   }
+
+  val associations: List[Association] = map.toList.flatMap {
+    case (node, labelsMap) => {
+      labelsMap.toList.map {
+        case (shapeLabel, info) => {
+          Association(RDFNodeSelector(node), shapeLabel, info)
+        }
+      }
+    }
+  }
+
+  override def addAssociation(a: Association): Either[String, ResultShapeMap] = {
+    a.nodeSelector match {
+      case RDFNodeSelector(node) => {
+        map.get(node) match {
+          case None => Right(ResultShapeMap(map.updated(node, Map(a.shapeLabel -> a.info))))
+          case Some(labelsMap) => {
+            labelsMap.get(a.shapeLabel) match {
+              case None => Right(ResultShapeMap(map.updated(node, labelsMap.updated(a.shapeLabel, a.info))))
+              case Some(info) =>
+                if (info.status == a.info.status) Right(this)
+                else Left(s"Cannot add association with contradictory status: Association: ${a}, Labels map: ${labelsMap}")
+            }
+          }
+        }
+      }
+      case _ => Left(s"Only RDFNode's can be added as associations to fixedShapeMaps. Value = ${a.nodeSelector}")
+    }
+  }
+
 }
 
 object ResultShapeMap {
