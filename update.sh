280beef54ddf23a7bd8280315cf39e2370b3ea52
Jose Labra <jelabra@gmail.com>
2018-03-03 08:18:30 +0100

    Updated API

---
+++ update.sh
@@ -0,0 +1,17 @@
+# set oldversion=0.0.0
+# set version=0.0.1
+
+# set source=\src\shapes\shaclex\target\universal
+# set dest=\gh-pages\shaclex\binaries
+
+# rm %dest%\shaclex-%oldversion%.zip
+# rm %dest%\shaclex-%oldversion%.tgz
+# cp %source%\shaclex-%version%.zip %dest%\shaclex-%version%.zip
+# cp %source%\shaclex-%version%.tgz %dest%\shaclex-%version%.tgz
+# sed -i "s|%oldversion%|%version%|g" index.html
+# chmod +w index.html
+rm -rf latest/api/*
+cp -R ~/src/shapes/shaclex/target/scala-2.11/api/* latest/api
+git add -A
+git commit -m "Updated API"
+git push
