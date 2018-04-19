fc5b0c599a62b9a3e57b939e590348c069082dc2
Jose Emilio Labra Gayo <labra@uniovi.es>
2018-04-19 14:17:41 +0200

    More steps in UML

--- modules/converter/src/main/scala/es/weso/shex/converter/ShEx2UML.scala
+++ modules/converter/src/main/scala/es/weso/shex/converter/ShEx2UML.scala
@@ -4,8 +4,8 @@ import cats._
 import cats.data._
 import cats.implicits._
 import cats.data.{EitherT, State, StateT}
-import es.weso.shex.Schema
-import es.weso.uml.UMLDiagram.UML
+import es.weso.shex.{Schema, ShapeExpr}
+import es.weso.uml.UMLDiagram.{UML, UMLClass}
 
 object ShEx2UML {
 
@@ -18,17 +18,31 @@ object ShEx2UML {
   def err[A](s: String): Converter[A] =
     EitherT.left[A](State.pure(s))
 
+  def modify(fn: UML => UML): Converter[Unit] =
+    EitherT.liftF(State.modify(fn))
+
   def schema2Uml(schema: Schema): Either[String,UML] = {
     val (uml, maybe) = cnvSchema(schema).value.run(UML.empty).value
     maybe.map(_ => uml)
   }
 
   def cnvSchema(schema: Schema): Converter[Unit] = {
-    schema.shapes.match {
+    schema.shapes match {
       case None => err(s"No shapes in schema")
-      case Some(shapes) => ???
+      case Some(shapes) => {
+        def cmb(x: Unit, s: ShapeExpr): Converter[Unit] = for {
+          cls <- cnvShapeExpr(s)
+          _ <- modify(_.addClass(cls))
+        } yield (())
+        shapes.foldM(())(cmb)
+      }
     }
   }
+
+ def cnvShapeExpr(se: ShapeExpr): Converter[UMLClass] = se.id match {
+   case None => ???
+   case Some(label) => ???
+ }
 
 
 }
